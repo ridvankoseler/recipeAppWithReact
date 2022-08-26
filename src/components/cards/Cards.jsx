d7b99ffebacb0d4c3b250fc2df61bce021528032
ridvankoseler <ridvankoseler@gmail.com>
2022-08-27 00:25:44 +0300

    Uptade8

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
-const Cards = () => {
+import { Card, Header, Image, MainContainer ,Button } from './Cards.styled'
+import defaultImage from '../../assets/default-image.jpg'
+import { useNavigate } from 'react-router-dom'
+const Cards = ({recipes}) => {
+  const navigate = useNavigate();
   return (
-    <div>Cards</div>
+    <MainContainer wrap='wrap'>
+      {
+        recipes.map(({recipe},index)=>{
+          const {label} = recipe
+          return (
+            <Card >
+              <Header>{label}</Header>
+              <Image src={recipe.image || defaultImage} />
+              <Button onClick={()=>navigate('detail', {state:recipe ,replace:false})}>View More</Button>
+              {/* //replace history siler */}
+             </Card>
+          )
+        })
+      }
+      
+
+      
+    </MainContainer>
   )
 }
 
